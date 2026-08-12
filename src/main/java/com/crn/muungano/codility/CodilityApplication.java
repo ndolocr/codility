4387262fce1cc03027736843e900709107c5aab7
raphaelndolo <raphael.ndolo@equitybank.co.ke>
2026-08-12 05:37:39 +0200

    Project Initiation

---
+++ src/main/java/com/crn/muungano/codility/CodilityApplication.java
@@ -0,0 +1,13 @@
+package com.crn.muungano.codility;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CodilityApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CodilityApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/crn/muungano/codility/CodilityApplicationTests.java
@@ -0,0 +1,13 @@
+package com.crn.muungano.codility;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CodilityApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
